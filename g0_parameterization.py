08848e0924cb0397331dd4148c18144948dd3242
Jales Bussinguer <jalesbussinguer@gmail.com>
2023-05-03 22:19:56 -0300

    added the loglikelihood method

--- g0_parameterization.py
+++ g0_parameterization.py
@@ -1,13 +1,18 @@
 # Code to derive the G0 parameters given a set of samples from SAR images
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+from scipy.special import gamma
+from scipy.optimize import minimize
 
 samples = pd.read_csv('D:/thesis_data/VEG_INDICES/samples/stratified/campestre/20m/FC_20170112_20m_patches.csv', sep=',')
 
 dprvi_samples = samples['dprvi_patch_0'].values
 
-def g0_estimator(samples, L):
+def g0_estimator_moments(samples, L):
+
+    # This function derives the parameters alpha and gamma from a G0 distribution by the moments method
+    # This method assumes the Number of Looks (L) as known (for Sentinel-1: L = 4)
 
     m1 = np.mean(samples)
     m2 = np.mean(samples**2)
@@ -19,5 +24,24 @@ def g0_estimator(samples, L):
 
     return round(a, 6), round(g, 6)
 
-alpha, gamma = g0_estimator(dprvi_samples,  L=4)
+alpha0, gamma0 = g0_estimator_moments(dprvi_samples,  L=4)
 
+def loglikelihood_estimator(chute_inicial):
+
+    # This function derives the parameters alpha and gamma from a G0 distribution by the maximum likelihhod method
+    # The input for this function is the alpha and gamma estimated previously by the moments method
+    # This method assumes the Number of Looks (L) as known (for Sentinel-1: L = 4)
+    # Samples is an array containing the pixels values of a given region in the SAR image
+
+    p_alpha = chute_inicial[0]
+    p_gamma = chute_inicial[1]
+
+    samples = dprvi_samples
+    n = len(samples) # Number of samples
+
+    return n * (gamma(4 - p_alpha) - p_alpha * np.log(p_gamma) - gamma(-p_alpha)) + (p_alpha-4) * sum(np.log(p_gamma + samples * 4))
+
+chute_inicial = [alpha0, gamma0]
+
+results = minimize(loglikelihood_estimator, x0=chute_inicial, method='BFGS')
+print(f'a = {results.x[0]}, g = {results.x[1]}')
